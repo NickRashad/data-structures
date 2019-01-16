626af0311b8fa01c481458d6b864ec3d2b6ea869
NickRashad <nrsternes@gmail.com>
2019-01-16 00:53:01 -0600

    Complete size and push functions

--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -6,13 +6,27 @@ var Stack = function() {
 
   // Implement the methods below
   someInstance.push = function(value) {
+    //Get the size of the object and add 1
+    var num = someInstance.size() + 1 ;
+    //Place the value on the object as a property and use a # as a key
+    return storage[num] = value;
   };
 
   someInstance.pop = function() {
+    //Get the size of the object to determine what key:value needs to be removed
+    var num = someInstance.size();
+    //Delete object at num
+    delete storage[num];
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
 };
+
+
+/*
+
+*/
