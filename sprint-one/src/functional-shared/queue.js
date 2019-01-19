4b783e86dcef08235fddedeaf929b9e507a5ed03
NickRashad <nrsternes@gmail.com>
2019-01-18 21:50:58 -0600

    Complete functional-shared stack and queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,42 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+  extend(storage, queueMethods);
+
+  return storage;
 };
 
+var extend = function (to, from) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
 var queueMethods = {};
 
+//Explicitly define or borrow methods
+queueMethods.enqueue = function (value) {
+  //Of all the numerical keys find the max value and increment by 1
+  var last = Math.max(Object.keys(this)
+    .filter(o => {
+      return !isNaN(parseInt(o));
+    })) + 1;
+  return this[last] = value;
+};
+queueMethods.dequeue = function () {
+  var first = Math.min(...Object.keys(this)
+    .filter(o => {
+      return !isNaN(parseInt(o));
+    }).map(o => parseInt(o))
+  );
 
+  var val = this[first];
+  delete this[first];
+  return val;
+};
+queueMethods.size = function () {
+  return Object.keys(this)
+    .filter(o => {
+      return !isNaN(parseInt(o));
+    }).length;
+};
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,40 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = {};
+
+  extend(storage, stackMethods);
+
+  return storage;
+};
+
+var extend = function (to, from) {
+  for (var key in from) {
+    return to[key] = from[key];
+  }
 };
 
 var stackMethods = {};
 
+stackMethods.push = function (value) {
+  //Push the value onto storage with a numerical key
+  var num = parseInt(this.size()) + 1;
+  return this[num] = value;
+};
+
+stackMethods.pop = function () {
+  //Find the last numerical
+  var num = this.size();
+  var val = this[num];
+  delete this[num];
+  return val;
+};
+
+
+stackMethods.size = function() {
+  return Object.keys(this).map(o => {
+    return isNaN(parseInt(o)) ? o : parseInt(o);
+  }).filter(o=>typeof o === 'number')
+    .length;
+};
 
