be2db824c791e9af9f03cd66ea82433c8d432ccc
NickRashad <nrsternes@gmail.com>
2019-01-18 23:38:24 -0600

    Complete prototypal stack and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,28 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = Object.create(queueMethods);
+  return storage;
 };
 
 var queueMethods = {};
 
+queueMethods.enqueue = function (value) {
+  if (this.size()) {
+    var num = Math.max(...Object.keys(this)) + 1;
+  } else {
+    var num = 1;
+  }
+  return this[num] = value;
+};
 
+queueMethods.dequeue = function () {
+  var num = Math.min(...Object.keys(this));
+  var val = this[num];
+  delete this[num];
+  return val;
+};
+
+queueMethods.size = function () {
+  return Object.keys(this).length;
+};
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,26 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  var storage = Object.create(stackMethods);
+  return storage;
 };
 
 var stackMethods = {};
 
+stackMethods.push = function (value) {
+  var num = this.size() + 1;
+  return this[num] = value;
+};
 
+stackMethods.pop = function () {
+  var num = this.size();
+  var val = this[num];
+  delete this[num];
+  return val;
+};
+
+stackMethods.size = function() {
+  return Object.keys(this)
+    .filter(o => !isNaN(parseInt(o)))
+    .length;
+};
