caf6e3659667076c4a0880eadf4c5ec26564b5dc
NickRashad <nrsternes@gmail.com>
2019-01-22 21:55:37 -0600

    Adjusted Pseudocode

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,32 +4,30 @@ var LinkedList = function() {
   list.tail = null;
 
   list.addToTail = function(value) {
-    // create new node and assign to variable
+    // Create new node and assign to variable
     var newNode = Node(value);
-    // check to see if this is first item
+    // Check to see if this is first item
     if (list.head === null) {
-      // update head
-      list.head = newNode; // create new Node and pass in value
-    } else {
-      // set node.next to equal current tail (list.tail)
+      // Update head to new Node
+      list.head = newNode;
+    } else { // Set previous tail.next value to equal current tail
       list.tail.next = newNode;
-    }
+    } // Always set tail to
     list.tail = newNode;
   };
 
   list.removeHead = function() {
-    // create var to hold head val
-    // head should become list.head.next
+    // Create var to hold former head value
     var returnData = list.head.value;
-    // update current head by looking at the node.next value of current head
-    list.head = list.head.next; // get the current head value
-    // return head value var
+    // Update current head by looking at the node.next value of current head
+    list.head = list.head.next;
+    // Return former head value
     return returnData;
   };
 
   list.contains = function(target) {
+    //Recurse over linked list to find target and return boolean
     var find = function(node) {
-      // debugger;
       if (node.value === target) {
         return true;
       } else if (node.value !== target && node.next !== null) {
@@ -39,7 +37,7 @@ var LinkedList = function() {
       }
     };
     return find(list.head);
-    // end function
+    // End function
   };
   return list;
 };
@@ -56,14 +54,3 @@ var Node = function(value) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-var linkedList = LinkedList();
-
-linkedList.addToTail(4);
-linkedList.addToTail(5);
-linkedList.addToTail(6);
-linkedList.addToTail(7);
-linkedList.head;
-linkedList.tail;
-//linkedList.removeHead();
-console.log(linkedList.head.value, linkedList);
