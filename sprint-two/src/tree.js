30700241709d88d34c45cd9f951bf1080c149c5b
Bruce Johnson <bruce@yogainternational.com>
2019-01-23 21:36:07 -0500

    Complete Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,18 +2,54 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null; // fix me
+  extend(treeMethods, newTree);
+  newTree.children = []; // fix me
 
   return newTree;
 };
 
+var extend = function(from, to) {
+  for (var key in from) {
+    to[key] = from[key];
+  }
+};
+
 var treeMethods = {};
 
-treeMethods.addChild = function(value) {};
+treeMethods.addChild = function(value) {
+  // var newNode = newTree;
+  // newNode.value = value;
+  //For every new addchild we attach a .value and .children
+  // console.log(this);
+  var node = {
+    children: [],
+    value: value,
+    addChild: treeMethods.addChild,
+    contains: treeMethods.contains
+  };
 
-treeMethods.contains = function(target) {};
+  return this.children.push(node);
+};
+
+treeMethods.contains = function(target) {
+  // children[i].value ? target
+  var status = false;
+  var recursion = function(currChild) {
+    for (var i = 0; i < currChild.length; i++) {
+      if (currChild[i].value === target) {
+        status = true;
+      } else {
+        recursion(currChild[i].children);
+      }
+    }
+  };
+  recursion(this.children);
+  return status;
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
+  extend: O(n) - Linear
+  addChild: O(1) - Constant
+  contains: either O(n^2) quadratic or linear O(n)
  */
