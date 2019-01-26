04f7f9bc272db2e2dcabddfee7c270a59a3df7aa
NickRashad <nrsternes@gmail.com>
2019-01-26 11:52:14 -0600

    Passed first test involving insert and retrieve

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -1,25 +1,54 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
-  //new Array(this.limit);
 };
 
 HashTable.prototype.insert = function(k, v) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   // Build the tuple var
   var tuple = [k, v];
-  // If bucket exists
-  // Iterate over current tuples in bucket
-  // If tuple at 0 matches k then
-  // Update tuple at 1 to v
-  // Else
-  // Push the tuple into bucket
+  // this._storage.set(index, tuple); //index value
+  // var tupleHolder = this._storage.get(index);
 
-  // Else build bucket then push tuple
+  // If bucket exists
+  // console.log(this._storage.get(index));
+  var bucket = this._storage.get(index);
+  if (Array.isArray(bucket) === true) {
+    // Iterate over current tuples in bucket
+    var status = true;
+    bucket.forEach(function(item, ind) {
+      var currentK = bucket[item][0];
+      var currentV = bucket[item][1];
+      // If tuple at 0 matches k then
+      if (currentK === k) {
+        // Update tuple at 1 to v
+        currentV = v;
+        status = false;
+      }
+      // Otherwise
+      if (ind === bucket.length - 1 && status) {
+        // Push the tuple into bucket
+        this._storage.set(bucket, tuple);
+      }
+    });
+  } else if (bucket === undefined) {
+    // Else build bucket then push tuple
+    this._storage.set(index, [tuple]);
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var tupleHolder = this._storage.get(index);
+  //iterate over tupleHolder
+  var result = undefined;
+  tupleHolder.forEach((item, ind, arr) => {
+    if (item[0] === k) {
+      result = item[1];
+    }
+  });
+  return result;
+  // return tupleHolder[1];
   // Access bucket if it exists
   // if( storage[index][0] )
   // Create variable called returnValue set to undefined
